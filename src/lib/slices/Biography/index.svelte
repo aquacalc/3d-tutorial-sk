e57736dd2ab66af0c0a0f4932bd780a3ae06a0a6
aquacalc <aquacalc@yahoo.com>
2024-08-10 19:32:32 -0400

    Heading.svelte

--- src/lib/slices/Biography/index.svelte
+++ src/lib/slices/Biography/index.svelte
@@ -4,17 +4,19 @@
 	import Button from '$lib/components/Button.svelte';
 	import Bounded from '$lib/components/Bounded.svelte';
 	import Avatar from './Avatar.svelte';
+	import Heading from '$lib/components/Heading.svelte';
 
 	export let slice: Content.BiographySlice;
 </script>
 
-<Bounded data-slice-type={slice.slice_type} 
-	data-slice-variation={slice.variation}>
-	<div class="grid gap-x-8 gap-y-6 
-		md:grid-cols-[2fr,1fr]">
-		<h1>
+<Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<div
+		class="grid gap-x-8 gap-y-6
+		md:grid-cols-[2fr,1fr]"
+	>
+		<Heading size="xl" class="col-start-1">
 			{slice.primary.heading}
-		</h1>
+		</Heading>
 
 		<div class="col-start-1 prose prose-invert prose-slate prose-xl">
 			<PrismicRichText field={slice.primary.description} />
